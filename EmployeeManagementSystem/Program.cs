9b0e676a5e5da7cab5d685fd7ea4df76ec08db98
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-10-10 15:23:12 +0530

    [Riya] Add : Employee Management System

--- EmployeeManagementSystem/Program.cs
+++ EmployeeManagementSystem/Program.cs
@@ -1,25 +1,34 @@
+using EmployeeManager.IManager;
+using EmployeeManager.Manager;
+using EmployeeRepository.EmpRepository;
+using EmployeeRepository.IEmpRepository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IEmpRepo,EmpRepo>();
+builder.Services.AddSingleton<IEmpManager,EmpManager>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Employee}/{action=AddEmployee}/{id?}");
 
 app.Run();
---
+++ EmployeeManagementSystem/Views/Employee/AddEmployee.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmployeeManagementSystem.Views.Employee
+{
+    public class AddEmployeeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
